09f913ce6060136a00fd293e5711380bda275435
= <=>
2025-06-03 22:02:08 -0300

    arreglo de program.cs

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,70 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using creadorTareas;
 int decision;
-int id;
+Tarea tarea = new Tarea();
 List<Tarea> tareasPendientes = new List<Tarea>();
 List<Tarea> tareasRealizadas = new List<Tarea>();
-Console.WriteLine("Ingrese cuantas tareas desea crear");
-int cantTareas = int.Parse(Console.ReadLine());
-for (int i = 0; i < cantTareas; i++)
-{
-    Tarea nuevaTarea = new Tarea();
-    nuevaTarea.cargarTarea();
-    tareasPendientes.Add(nuevaTarea);
-}
-
 do
 {
-    Console.WriteLine("[1] Marcar Tareas como Realizada");
-    Console.WriteLine("[2] Mostrar Tareas Pendientes");
-    Console.WriteLine("[3] Mostrar Tareas Realizadas");
-    Console.WriteLine("[4] Buscar Por Descripcion");
+    Console.WriteLine("[1] Agregar Tareas");
+    Console.WriteLine("[2] Marcar Tareas como Realizada");
+    Console.WriteLine("[3] Mostrar Tareas Pendientes");
+    Console.WriteLine("[4] Mostrar Tareas Realizadas");
+    Console.WriteLine("[5] Buscar Por Descripcion");
     Console.WriteLine("[0] Salir");
     decision = int.Parse(Console.ReadLine());
     switch (decision)
     {
         case 1:
-            Console.WriteLine("Ingrese el id de cual tarea quiere marcar como realizada");
-
-            id = int.Parse(Console.ReadLine());
-            for (int i = 0; i < tareasPendientes.Count; i++)
-            {
-                if (tareasPendientes[i].TareaID == id)
-                {
-                    tareasRealizadas.Add(tareasPendientes[i]);
-                    tareasPendientes.Remove(tareasPendientes[i]);
-                    Console.WriteLine("La tarea fue marcada como realizada");
-                }
-                else
-                {
-                    Console.WriteLine("La tarea con ese id no se pudo encontrar");
-                }
-            }
-
-            break;
-
+        tarea.anadirTarea(tarea, tareasPendientes);
+        break;
         case 2:
-            foreach (var tareasPen in tareasPendientes)
-            {
-                Console.WriteLine($"Id:{tareasPen.TareaID}");
-                Console.WriteLine($"Descripcion de la tarea:{tareasPen.Descripcion}");
-                Console.WriteLine($"Duracion:{tareasPen.Duracion}");
-            }
-            break;
+            tarea.marcarComoRealizada(tareasPendientes,tareasRealizadas);
+        break;
+
         case 3:
-            foreach (var tareasReal in tareasRealizadas)
-            {
-                Console.WriteLine($"Id:{tareasReal.TareaID}");
-                Console.WriteLine($"Descripcion de la tarea:{tareasReal.Descripcion}");
-                Console.WriteLine($"Duracion:{tareasReal.Duracion}");
-            }
-            break;
+           tarea.mostrarListaPen(tareasPendientes);
+        break;
         case 4:
-            Tarea buscarTarea = new Tarea();
-            buscarTarea.buscarTarea(tareasPendientes);
+            tarea.mostrarListaReal(tareasRealizadas);
             break;
-        
+        case 5:
+            tarea.buscarTarea(tareasPendientes);
+            break;
     }
-   
 } while (decision != 0);
--- ToDo/Tarea.cs
+++ ToDo/Tarea.cs
@@ -18,7 +18,7 @@ namespace creadorTareas
             get; set;
         }
 
-        public void cargarTarea()
+        public void crearTarea()
         {
             Console.Write("Ingrese el id de la tarea:");
             TareaID = int.Parse(Console.ReadLine());
@@ -33,6 +33,65 @@ namespace creadorTareas
                     Console.Write("Ingrese Una duracion valida:");
                     Duracion = int.Parse(Console.ReadLine());
                 } while (Duracion < 10 || Duracion > 100);
+            }
+        }
+        public void anadirTarea(Tarea tarea, List<Tarea> tareasPendientes)
+        {
+            Random aleatorio = new Random();
+            int cantTareas = aleatorio.Next();
+            for (int i = 0; i < cantTareas; i++)
+        {
+            tarea.crearTarea();
+            tareasPendientes.Add(tarea);
+        }
+        }
+
+
+        public void marcarComoRealizada(List<Tarea> tareasPendientes, List<Tarea> tareasRealizadas)
+        {
+            int id;
+            Console.WriteLine("Ingrese el id de cual tarea quiere marcar como realizada");
+            id = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tareasPendientes.Count; i++)
+            {
+                if (tareasPendientes[i].TareaID == id)
+                {
+                    tareasRealizadas.Add(tareasPendientes[i]);
+                    tareasPendientes.RemoveAt(i);
+                    Console.WriteLine("La tarea fue marcada como realizada");
+                }
+                else
+                {
+                    Console.WriteLine("La tarea con ese id no se pudo encontrar");
+                }
+            }
+        }
+
+        public void mostrarListaPen(List<Tarea> tareasPendientes)
+        {
+             foreach (var tareasPen in tareasPendientes)
+            {
+                Console.WriteLine($"Id:{tareasPen.TareaID}");
+                Console.WriteLine($"Descripcion de la tarea:{tareasPen.Descripcion}");
+                Console.WriteLine($"Duracion:{tareasPen.Duracion}");
+            }
+            if (tareasPendientes.Count == 0)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+        }
+
+        public void mostrarListaReal(List<Tarea> tareasRealizadas)
+        {
+            foreach (var tareasReal in tareasRealizadas)
+            {
+                Console.WriteLine($"Id:{tareasReal.TareaID}");
+                Console.WriteLine($"Descripcion de la tarea:{tareasReal.Descripcion}");
+                Console.WriteLine($"Duracion:{tareasReal.Duracion}");
+            }
+            if (tareasRealizadas.Count == 0)
+            {
+                Console.WriteLine("La lista esta vacia");
             }
         }
         public void buscarTarea(List<Tarea> tareasPendientes)
