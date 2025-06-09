c8d54bad10cf1341b607ac3bd5dc02845dddc813
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-06-09 12:13:00 -0300

    Inicio del tp

---
+++ ToDo/Tarea.cs
@@ -0,0 +1,20 @@
+namespace creadorTareas
+{
+    public class Tarea
+
+    {
+        public int TareaID
+        {
+            get; set;
+        }
+        public string Descripcion
+        {
+            get; set;
+        }
+        public int Duracion
+        {
+            get; set;
+        }
+
+    }
+}
